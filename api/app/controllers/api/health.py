d9111c396b276ba0e1b760e3cdfb6ef4416d11be
azzamsa <oktinastuzi@pm.me>
2020-04-10 09:44:30 +0700

    Add: `version` to health endpoint

--- api/app/controllers/api/health.py
+++ api/app/controllers/api/health.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from flask_restful import Resource
 
 from app.vendors.rest import response
@@ -5,5 +7,17 @@ from app.vendors.rest import response
 
 class HealthCheck(Resource):
     def get(self):
-        data = {"check": "100"}
+        version = ""
+
+        current_path = pathlib.Path(__file__)
+        path = current_path.parents[3].joinpath("version.txt")
+        if path.is_file():
+            with open(path, "rb") as f:
+                version = f.read().decode("utf-8")
+                version = version.rstrip()
+
+        if not version:
+            version = "__UNKNOWN__"
+
+        data = {"status": "running", "version": version}
         return response(200, data=data, message="OK")
